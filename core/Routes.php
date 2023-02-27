b1e2fdc9a9c2caf83bbf5906e862572e130c4d56
samuel dias <93612090+SDias14@users.noreply.github.com>
2023-02-27 12:03:44 -0300

    parado na instanciação da classe routes em index.php

---
+++ core/Routes.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Core;
+
+class Routes{
+
+    /** 
+     * var url 
+     */
+
+   
+     private string $url;
+    
+
+    public function __construct(){
+
+    }
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MV Embalagens</title>
+</head>
+<body>
+    
+    <?php
+
+    /**
+     * incluindo o autoload do composer para carregar as classes
+     */
+    
+     require './vendor/autoload.php';
+
+    /**
+     * instanciando a classe Routes
+     */
+
+    $routes = new \CORE\Routes();
+
+
+
+
+    ?>
+</body>
+</html>
